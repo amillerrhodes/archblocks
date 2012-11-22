fc0aba76f4268da500fb3dac11037592d0c21e40
Anthony Miller-Rhodes <amillerrhodes@gmail.com>
2012-11-22 02:08:45 -0500

    Modified mbr_default.sh and bios_grub2.sh

---
+++ blocks/bootloader/bios_grub2.sh
@@ -0,0 +1,4 @@
+_install_pkg grub-bios
+grub-install --target=i386-pc --recheck /dev/sda
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
---
+++ blocks/filesystem/mbr_default.sh
@@ -0,0 +1,44 @@
+_drivequery;
+
+BOOT_DRIVE=$INSTALL_DRIVE # expected format /dev/sda
+PARTITION_EFI_BOOT=0
+LABEL_SWAP=swap
+LABEL_ROOT=root
+MOUNT_PATH=/mnt
+
+
+_filesystem_pre_baseinstall () {
+_countdown 10 "ERASING $INSTALL_DRIVE"
+
+dd if=/dev/zero of=/dev/sda bs=512 count=1
+sfdisk ${INSTALL_DRIVE} << EOF
+,,L
+,19,S
+EOF
+
+# make filesystems
+mkswap ${INSTALL_DRIVE}${PARTITION_SWAP}
+swapon ${INSTALL_DRIVE}${PARTITION_SWAP}
+mkfs.ext4 ${INSTALL_DRIVE}${PARTITION_ROOT}
+# mkswap /dev/sda2
+
+# mount target
+mkdir -p ${MOUNT_PATH}
+mount ${INSTALL_DRIVE}${PARTITION_ROOT} ${MOUNT_PATH}
+}
+
+_filesystem_post_baseinstall () {
+# not using genfstab here since it doesn't record partlabel labels
+genfstab -U -p /mnt >> /mnt/etc/fstab
+}
+
+_filesystem_pre_chroot ()
+{
+}
+
+_filesystem_post_chroot ()
+{
+# KERNEL_PARAMS used by BOOTLOADER
+# KERNEL_PARAMS="${KERNEL_PARAMS:+${KERNEL_PARAMS} }cryptdevice=/dev/sda3:${LABEL_ROOT_CRYPT} root=/dev/mapper/${LABEL_ROOT_CRYPT} ro rootfstype=ext4"
+KERNEL_PARAMS="${KERNEL_PARAMS:+${KERNEL_PARAMS} }root=UUID=$(_get_uuid ${INSTALL_DRIVE}${PARTITION_ROOT}) ro rootfstype=ext4"
+}
